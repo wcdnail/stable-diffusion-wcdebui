7a017c372d026aa58d8e424d531e7158e8140583
Nitro <mail@host.dom>
2023-02-11 18:29:07 +0300

    ++

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,10 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--opt-split-attention --medvram
+
+rem --listen
+rem --skip-python-version-check
+rem set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 
 call webui.bat
