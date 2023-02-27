2bc5d277a5bb8d1dfcef36b8daa1dd4ad2641dd8
Mike Nitro <miken@datasunrise.com>
2023-02-27 10:42:39 +0300

    +++

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--medvram --xformers --enable-console-prompts --theme "dark"
+set COMMANDLINE_ARGS=--medvram --xformers --enable-console-prompts --theme dark
 
 rem 
 
