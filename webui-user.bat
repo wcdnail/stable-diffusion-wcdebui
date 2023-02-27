87b3bc7e4e37730c1886c59651ef190704fd0f61
Nitro <mail@host.dom>
2023-02-27 18:32:49 +0300

    ++

--- webui-user.bat
+++ webui-user.bat
@@ -3,11 +3,12 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --allow-code --enable-console-prompts --no-half --precision full --theme dark
+set COMMANDLINE_ARGS=--xformers --allow-code --enable-console-prompts --theme dark
 
 rem  --opt-split-attention
 rem --theme dark
 rem --listen
 rem --skip-python-version-check
+rem -- Жор слишком велик для 12Гб VRAM -- --no-half --precision full 
 
 call webui.bat
